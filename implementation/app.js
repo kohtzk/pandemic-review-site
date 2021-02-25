e47947ea17c717c014f8a04a81f7be023ebc088f
Kieran Kohtz <kohtzk@gmail.com>
2021-02-25 18:12:33 +0000

    Update app.js

--- implementation/app.js
+++ implementation/app.js
@@ -6,6 +6,38 @@ const app = express();
 //listen for requests, uses localhost by default
 app.listen(3000);
 
+// request: { "username" : username, "password" : password}
+// response: { "result" : "failed" OR "some sort of unique ID" }
+app.post('/login', function (req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+    // check username and password is correct
+    let result = { "result": "failed" };
+    res.send(JSON.stringify(result));
+})
+
+// request: { "username" : username, "password" : password}
+// response: { "result" : "success" OR "failed" }
+app.post('/newaccount', function (req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+    // add new username and password is correct
+    let result = { "result": "failed" };
+    res.send(JSON.stringify(result));
+})
+
+// request: { "username" : username }
+// response: { "name" : name, "businessID" : businessid }                      
+app.post('/profile', function (req, res, next) {
+    let username = req.body.username;
+    // get user information
+    let profiledata = { 
+        "name": "Trevor Wood",
+        "businessID": "null"
+        };
+    res.send(JSON.stringify(profiledata));
+})
+
 app.get('/', (req, res) => {
     res.sendFile('frontend/map.html', { root: __dirname });
 });

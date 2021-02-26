156f4dde2a83d9755f23e001894fd40ca4106651
ZBC043 <46783675+ZBC043@users.noreply.github.com>
2021-02-26 16:57:04 +0000

    Add files via upload
    Business webpage private view and add review for businesses.

---
+++ implementation/frontend/Bochen/ReviewLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export default function ReviewLayout(props){
+
+    return(
+        <div className = "Font">
+            <h1 className = "businessName">Business Rereview for {props.review.name}</h1>
+            <h1 className = "informationDescription">{props.review.description}</h1>
+            <button className = "reviewButton">"Add a review"</button>
+            <input className = "reviewText" type="text"/>
+        </div>
+    );
+}

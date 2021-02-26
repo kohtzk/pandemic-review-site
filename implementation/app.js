13656311c4ff83c86f7af0b266670a0068045dd8
Kieran Kohtz <kohtzk@gmail.com>
2021-02-26 14:16:10 +0000

    added some more dummy request handlers

--- implementation/app.js
+++ implementation/app.js
@@ -7,16 +7,6 @@ const app = express();
 app.listen(3000);
 
 // request: { "username" : username, "password" : password}
-// response: { "result" : "failed" OR "some sort of unique ID" }
-app.post('/login', function (req, res, next) {
-    let username = req.body.username;
-    let password = req.body.password;
-    // check username and password is correct
-    let result = { "result": "failed" };
-    res.send(JSON.stringify(result));
-})
-
-// request: { "username" : username, "password" : password}
 // response: { "result" : "success" OR "failed" }
 app.post('/newaccount', function (req, res, next) {
     let username = req.body.username;
@@ -26,16 +16,70 @@ app.post('/newaccount', function (req, res, next) {
     res.send(JSON.stringify(result));
 })
 
-// request: { "username" : username }
-// response: { "name" : name, "businessID" : businessid }                      
+// request: { "businessname" : businessname, "businessowner" : userid, "contact" : { "phone" : number, "email" : email }, 
+//            "location" : { "number" : number, "street" : street, "postcode" : postcode }, "type" : businesstype }
+// response: { "result" : "success" OR "failed" }
+app.post('/addbusiness', function (req, res, next) {
+    let review = req.body;
+    // put data in database
+    let result = { "success": true };
+    res.send(JSON.stringify(result));
+})
+
+// request: { "user" : userid, "business" : businessid, "oneway" : rating, "sanitizer" : rating, "personlimit" : rating, 
+//            "masks" : rating, "bouncers" : rating, "tempcheck" : rating, "staffppe" : rating }
+// response: { "result" : "success" OR "failed" }
+app.post('/addreview', function (req, res, next) {
+    let review = req.body;
+    // put data in database
+    let result = { "result": "success" };
+    res.send(JSON.stringify(result));
+})
+
+// request: { "username" : username, "password" : password}
+// response: { "result" : "failed" OR "some sort of unique ID" }
+app.post('/login', function (req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+    // check username and password is correct
+    let result = { "result": "failed" };
+    res.send(JSON.stringify(result));
+})
+
+// request: { "userid" : userid }
+// response: { "username" : username, "name" : name, "businessID" : businessid }                      
 app.post('/profile', function (req, res, next) {
     let username = req.body.username;
     // get user information
     let profiledata = { 
+        "username" : "theTree",
         "name": "Trevor Wood",
         "businessID": "null"
         };
     res.send(JSON.stringify(profiledata));
+})
+
+// request: { "businessid" : businessid }
+// response: { "username" : username, "name" : name, "businessID" : businessid }                      
+app.post('/profile', function (req, res, next) {
+    let username = req.body.username;
+    // get user information
+    let profiledata = { 
+        "username" : "theTree",
+        "name": "Trevor Wood",
+        "businessID": "null"
+        };
+    res.send(JSON.stringify(profiledata));
+})
+
+// request: { "businessid" : businessid }
+// response: { "businessname" : businessname, "businessowner" : userid, "contact" : { "phone" : number, "email" : email }, 
+//             "location" : { "number" : number, "street" : street, "postcode" : postcode }, "type" : businesstype }
+app.post('/getreviews', function (req, res, next) {
+    let business = req.body.businessid;
+    // get data from database
+    let reviews = { "reviews": [] };
+    res.send(JSON.stringify(reviews));
 })
 
 app.get('/', (req, res) => {

9143ddf5b08f2568020fec7bafe81446437530bc
TheTrueTree <58667461+TheTrueTree@users.noreply.github.com>
2021-03-07 18:47:48 +0000

    Less Janky
    Used a callback to make database query less jank

--- implementation/database.js
+++ implementation/database.js
@@ -1,6 +1,5 @@
 const sqlite = require('sqlite3');
 var db = new sqlite.Database('./pandemicDatabase.db');
-var promise = require('promise')
 
 //Input required => username & password.
 //Return => Whether combo exists
@@ -10,22 +9,19 @@ function checkName(email, password) {
     db.each("SELECT Email emailS, Password pword FROM Users WHERE Email  = ?", [email], (err, row) => {
         emailD = row.emailS;
         passwD = row.pword;
-    })
-    setTimeout(() => {
+    }, () => {
         if(emailD == email){
             if(passwD == password){console.log("Correct")}
             else{console.log("Wrong PW")}
         } else {console.log("Email not found")}
-    },10)
+    })
 }
 
 function createUser(name, email, password){
     db.run("INSERT INTO Users(Name, Email, Password) VALUES(?, ?, ?)", [name, email, password], function(err) {
         if (err) {
-            console.log(err.message);
+            console.log("Email already exists!");
         }
-        // get the last insert id
-        console.log(`A row has been inserted with rowid ${this.lastID}`);
     })
 }
 

9a81f57e24eb16f5f532f5dc7d5052f8324877bf
ZBC043 <bz470@bath.ac.uk>
2021-03-14 14:00:56 +0000

    added more stuff for add review page

--- implementation/frontend/src/components/ReviewLayout.js
+++ implementation/frontend/src/components/ReviewLayout.js
@@ -9,14 +9,32 @@ export default function ReviewLayout(props) {
       </div>
       <div className="informationDescription">
         <h1>{props.review.description1}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description2}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description3}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description4}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description5}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description6}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description7}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description8}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
         <h1>{props.review.description9}</h1>
+        <button className = "addReviewButton">Yes</button>
+        <button className = "addReviewButton">No</button>
       </div>
       <div>
         <button className="reviewButton">Add written review</button>

a6353d6241c52c76c00302fddd102824b2c47323
ZBC043 <bz470@bath.ac.uk>
2021-03-18 17:24:28 +0000

    Change css

--- implementation/frontend/src/pages/ViewBusinessProperty.js
+++ implementation/frontend/src/pages/ViewBusinessProperty.js
@@ -1,21 +1,50 @@
 import * as React from "react";
 import BusinessProperty from "../components/BusinessProperty";
 import businessData from "../components/businessDataTwo";
+import { getBusiness } from "../services/getBusiness";
+import { getReviews } from "../services/getReviews";
+import BusinessReviews from "../components/BusinessReviews";
 
 class ViewBusinessProperty extends React.Component {
   constructor() {
     super();
     this.state = {
       data: businessData,
+      businessID: 0,
+      gettingBusinessData: null,
+      gettingReviewData: null,
     };
   }
 
-  render() {
-    const businesses = this.state.data.map((business) => (
-      <BusinessProperty key={business.id} business={business} />
-    ));
+  async componentDidMount(){
+    await getBusiness({"id" : this.state.businessID})
+    .then((response) => {
+      this.setState({gettingBusinessData:response});
+    });
 
-    return <div>{businesses}</div>;
+    await getReviews({"user_id" : null, "business_id" : this.state.businessID})
+    .then((response) => {
+      this.setState({gettingReviewData:response});
+    });
+  }
+
+  render() {
+    if(this.state.gettingBusinessData == null || this.state.gettingReviewData == null){
+      return null;
+    }
+    else if(this.state.gettingBusinessData != null || this.state.gettingReviewData != null){
+      const businesses = this.state.data.map((business) => (
+        <BusinessProperty key={business.id} business={business} />
+      ));
+
+      return(
+        <div>
+          {businesses}
+
+        </div>
+      );
+    }
+    
   }
 }
 

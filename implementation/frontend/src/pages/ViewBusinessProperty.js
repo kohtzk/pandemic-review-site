0ccba03f013ca48c6f22b39fac8d964e9c9d1103
ZBC043 <bz470@bath.ac.uk>
2021-03-21 14:46:26 +0000

    Pull business profile from database

--- implementation/frontend/src/pages/ViewBusinessProperty.js
+++ implementation/frontend/src/pages/ViewBusinessProperty.js
@@ -1,24 +1,26 @@
 import * as React from "react";
 import BusinessProperty from "../components/BusinessProperty";
-import businessData from "../components/businessDataTwo";
-import { getBusiness } from "../services/getBusiness";
-import { getReviews } from "../services/getReviews";
-import "../pages/BusinessProperty.css";
 import PublicBusinessInformation from "../components/PublicBusinessInformation";
+import DisplyBusinessAverageRating from "../components/DisplayBusinessAverageRating";
+import NoSuchBusiness from "../components/NoSuchBusiness";
+import { getReviews } from "../services/getReviews";
+import { getBusinessProfile } from "../services/getBusinessProfile";
+import { getAverageReview } from "../services/getAverageReview";
+import "../pages/BusinessProperty.css";
 
 class ViewBusinessProperty extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: businessData,
-      businessID: 0,
+      businessID: null,
       gettingBusinessData: null,
       gettingReviewData: null,
+      gettingAverageRating: null,
     };
   }
 
   async componentDidMount(){
-    await getBusiness({"id" : this.state.businessID})
+    await getBusinessProfile({"business_id" : this.state.businessID})
     .then((response) => {
       this.setState({gettingBusinessData:response});
     });
@@ -27,25 +29,40 @@ class ViewBusinessProperty extends React.Component {
     .then((response) => {
       this.setState({gettingReviewData:response});
     });
+
+    await getAverageReview({"user_id" : null, "business_id" : this.state.businessID})
+    .then((response) => {
+      this.setState({gettingAverageRating:response});
+    });
   }
 
   render() {
     if(this.state.gettingBusinessData == null || this.state.gettingReviewData == null){
       return null;
     }
+    else if(this.state.businessID == null){
+      return(<NoSuchBusiness />)
+    }
     else if(this.state.gettingBusinessData != null || this.state.gettingReviewData != null){
-      const businesses = this.state.data.map((business) => (
-        <BusinessProperty key={business.id} business={business} />
+      const businessProfile = this.state.gettingBusinessData.data.map((business) => (
+        <BusinessProperty key={business.business_id} businessProperty={business} />
       ));
 
       const displayAllReviews = this.state.gettingReviewData.data.map((review) => (
         <PublicBusinessInformation key={review.business_id} publicBusinessInformation={review} />
       ));
 
+      const businessAverageRating = this.state.gettingAverageRating.data.map((rating) => (
+        <DisplyBusinessAverageRating key={rating.business_id} displyBusinessAverageRating={rating} />
+      ))
+
       return(
         <div>
           <div>
-            {businesses}
+            {businessProfile}
+          </div>
+          <div>
+            {businessAverageRating}
           </div>
           <div className="displayAllReviews">
             {displayAllReviews}

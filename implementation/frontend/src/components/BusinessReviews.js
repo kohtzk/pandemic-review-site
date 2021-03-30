b86dbb71be77b483279da46cc6f0615dff327fe1
Lauren <lat.mail.lat@gmail.com>
2021-03-30 15:13:40 +0100

    report button

--- implementation/frontend/src/components/BusinessReviews.js
+++ implementation/frontend/src/components/BusinessReviews.js
@@ -11,10 +11,12 @@ class BusinessReviews extends React.Component {
 
   handleClick() {
     this.setState((prevState) => {
-      return {
-        submit: true, //NOT SUPPOSED TO ALTER DIRECTLY...
-        //CHECK OUT: https://scrimba.com/learn/learnreact/react-todo-app-phase-61--cMqr68Sr
-      };
+      alert("This review has been reported to the third party moderators")
+      // return {
+      //   submit: true, //NOT SUPPOSED TO ALTER DIRECTLY...
+      //   //CHECK OUT: https://scrimba.com/learn/learnreact/react-todo-app-phase-61--cMqr68Sr
+        
+      // };
     });
   }
   render() {
@@ -26,7 +28,7 @@ class BusinessReviews extends React.Component {
           <h3 className = "details">UserID: {this.props.B_reviewS.user_id}</h3>
           {/* <h3>Date of Review: {this.props.B_reviewS.date}</h3> */}
           <p className = "details2">Review text: {this.props.B_reviewS.text}</p>
-          {/* <button onClick={this.handleClick}>Delete Review</button>  */}
+          <div className = "details"><button onClick={this.handleClick }>Report Review</button> </div>
 
         </div>        
       </div>
